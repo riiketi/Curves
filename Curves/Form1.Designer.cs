554cb71a8b8d436065df4f32a807d4fc40392304
Tengiz Margvelashvili <thetenlight@gmail.com>
2019-11-20 09:00:38 +0500

    Добавьте файлы проекта.

---
+++ Curves/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Curves
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Browse_btn = new System.Windows.Forms.Button();
+            this.M_tb = new System.Windows.Forms.TextBox();
+            this.N_tb = new System.Windows.Forms.TextBox();
+            this.M_label = new System.Windows.Forms.Label();
+            this.N_label = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // Browse_btn
+            // 
+            this.Browse_btn.Location = new System.Drawing.Point(10, 6);
+            this.Browse_btn.Name = "Browse_btn";
+            this.Browse_btn.Size = new System.Drawing.Size(150, 20);
+            this.Browse_btn.TabIndex = 0;
+            this.Browse_btn.Text = "Обзор";
+            this.Browse_btn.UseVisualStyleBackColor = true;
+            this.Browse_btn.Click += new System.EventHandler(this.Browse_btn_Click);
+            // 
+            // M_tb
+            // 
+            this.M_tb.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.M_tb.Location = new System.Drawing.Point(310, 6);
+            this.M_tb.Name = "M_tb";
+            this.M_tb.ReadOnly = true;
+            this.M_tb.Size = new System.Drawing.Size(395, 20);
+            this.M_tb.TabIndex = 1;
+            // 
+            // N_tb
+            // 
+            this.N_tb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.N_tb.Location = new System.Drawing.Point(830, 6);
+            this.N_tb.Name = "N_tb";
+            this.N_tb.ReadOnly = true;
+            this.N_tb.Size = new System.Drawing.Size(160, 20);
+            this.N_tb.TabIndex = 2;
+            // 
+            // M_label
+            // 
+            this.M_label.AutoSize = true;
+            this.M_label.Location = new System.Drawing.Point(200, 9);
+            this.M_label.Name = "M_label";
+            this.M_label.Size = new System.Drawing.Size(101, 13);
+            this.M_label.TabIndex = 3;
+            this.M_label.Text = "Количество строк:";
+            // 
+            // N_label
+            // 
+            this.N_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.N_label.AutoSize = true;
+            this.N_label.Location = new System.Drawing.Point(720, 9);
+            this.N_label.Name = "N_label";
+            this.N_label.Size = new System.Drawing.Size(109, 13);
+            this.N_label.TabIndex = 4;
+            this.N_label.Text = "Количество кривых:";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.AutoScroll = true;
+            this.zedGraphControl1.AutoSize = true;
+            this.zedGraphControl1.IsAutoScrollRange = true;
+            this.zedGraphControl1.IsShowHScrollBar = true;
+            this.zedGraphControl1.IsShowVScrollBar = true;
+            this.zedGraphControl1.IsSynchronizeXAxes = true;
+            this.zedGraphControl1.IsSynchronizeYAxes = true;
+            this.zedGraphControl1.IsZoomOnMouseCenter = true;
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 32);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(984, 418);
+            this.zedGraphControl1.TabIndex = 5;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1008, 462);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.N_label);
+            this.Controls.Add(this.M_label);
+            this.Controls.Add(this.N_tb);
+            this.Controls.Add(this.M_tb);
+            this.Controls.Add(this.Browse_btn);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse_btn;
+        private System.Windows.Forms.TextBox M_tb;
+        private System.Windows.Forms.TextBox N_tb;
+        private System.Windows.Forms.Label M_label;
+        private System.Windows.Forms.Label N_label;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ Curves/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ZedGraph;
+
+
+namespace Curves
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string[] ColourValues = new string[] 
+        {
+            "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#000000",
+            "#800000", "#008000", "#000080", "#808000", "#800080", "#008080", "#808080",
+            "#C00000", "#00C000", "#0000C0", "#C0C000", "#C000C0", "#00C0C0", "#C0C0C0",
+            "#400000", "#004000", "#000040", "#404000", "#400040", "#004040", "#404040",
+            "#200000", "#002000", "#000020", "#202000", "#200020", "#002020", "#202020",
+            "#600000", "#006000", "#000060", "#606000", "#600060", "#006060", "#606060",
+            "#A00000", "#00A000", "#0000A0", "#A0A000", "#A000A0", "#00A0A0", "#A0A0A0",
+            "#E00000", "#00E000", "#0000E0", "#E0E000", "#E000E0", "#00E0E0", "#E0E0E0",
+        };
+
+        private void GetCoordinates(string[] lines, int rows, int cols)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            
+
+            // Очистим список кривых на тот случай, если до этого сигналы уже были нарисованы
+            pane.CurveList.Clear();
+
+            List<PointPairList> lists = new List<PointPairList>();
+            List<LineItem> myCurves = new List<LineItem>();
+
+            for (int j = 1; j < cols; ++j)    // создание Y-кривых
+                lists.Add(new PointPairList());
+
+            // заполняем список точек 
+            for (int i = 0; i < rows; ++i) {
+                string thisline = lines[i];
+                string[] values = thisline.Split(';');
+                if (values.Count() != cols)
+                    throw new FileLoadException( String.Format( "Data line {0} has {1} items instead of {2}", i+1, values.Count(), cols));
+                double depth = double.Parse(values[0], CultureInfo.InvariantCulture);// Глубина текущей строки
+                for (int j = 1; j < cols; ++j)
+                {
+                    double y = double.Parse(values[j], CultureInfo.InvariantCulture);
+                    lists[j-1].Add(depth, y);
+                }
+                
+            }
+
+            pane.YAxisList.Clear(); // Удалим существующие оси Y
+
+            int[] axis = new int[cols-1];
+
+            for (int j = 0; j < cols-1; ++j)    // Создадим N новых оси Y
+            {
+                axis[j] = pane.AddYAxis("Curve " + (j+1));
+                myCurves.Add(pane.AddCurve("Curve " + (j + 1), lists[j], ColorTranslator.FromHtml(ColourValues[j]), SymbolType.None));
+                myCurves[j].YAxisIndex = axis[j];
+                pane.YAxisList[axis[j]].Title.FontSpec.FontColor = ColorTranslator.FromHtml(ColourValues[j]);
+            }
+
+            pane.Legend.IsVisible = false;
+            pane.Title.IsVisible = false;
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void Browse_btn_Click(object sender, EventArgs e)
+        {
+            int Rows = -1;
+            int Cols = -1;
+            string text = "";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    text = File.ReadAllText(file);
+                    string[] lines = text.Split('\n');
+                    Rows = lines.Count();
+                    if (Rows == 0) throw new FileLoadException("File is empty");
+                    M_tb.Text = Convert.ToString(Rows);
+                    Cols = lines[0].Split(';').Count();
+                    N_tb.Text = Convert.ToString(Cols-1);
+                    GetCoordinates( lines, Rows, Cols);
+                }
+                catch (Exception ex) {
+                    MessageBox.Show( ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
